3c4961e68a4a9fc6fdcd10420f974dda66c9ecbb
ellen-rose-james <ellenrj03@gmail.com>
2025-01-15 01:44:48 +0530

    scroll up btn for event page

--- assets/js/global.js
+++ assets/js/global.js
@@ -41,6 +41,26 @@ AOS.init({
     offset: 20,
 });
 
+// Scroll up button
+let scrollUp = document.getElementById("scrollUp");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+   scrollUp.style.display = "block";
+  } else {
+   scrollUp.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // // Music Toggle Functionality
 // const backgroundMusic = document.getElementById('backgroundMusic');
 // const musicToggleButton = document.getElementById('musicToggleButton');
